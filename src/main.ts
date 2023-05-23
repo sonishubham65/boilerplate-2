ceb4d5e35f41fc9a07c914c1d9e1511750159c87
Shubham Soni <shubham.soni@mavq.com>
2023-05-23 18:51:23 +0530

    Added console

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { AppModule } from './app.module';
 import { ConfigService } from './modules/config/config.service';
 import { faker } from '@faker-js/faker';
 import axios from 'axios';
+console.log('process.env.NODE_CONFIG', process.env.NODE_CONFIG);
 process.env.NODE_CONFIG_STRICT_MODE = 'true';
 
 async function bootstrap() {
