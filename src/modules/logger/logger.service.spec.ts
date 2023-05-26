6b8bccc21271670564a81c7af7e104efb6fd4c60
Shubham Soni <shubham.soni@mavq.com>
2023-05-26 17:38:20 +0530

    refactor

--- src/modules/logger/logger.service.spec.ts
+++ src/modules/logger/logger.service.spec.ts
@@ -1,15 +1,22 @@
+import { REQUEST } from '@nestjs/core';
 import { Test, TestingModule } from '@nestjs/testing';
 import { LoggerService } from './logger.service';
 
 describe('LoggerService', () => {
-  let service: LoggerService;
+  let service: Promise<LoggerService>;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [LoggerService],
+      providers: [
+        LoggerService,
+        {
+          provide: REQUEST,
+          useValue: {},
+        },
+      ],
     }).compile();
 
-    service = module.get<LoggerService>(LoggerService);
+    service = module.resolve<LoggerService>(LoggerService);
   });
 
   it('should be defined', () => {
--- src/modules/user/user.service.spec.ts
+++ src/modules/user/user.service.spec.ts
@@ -1,12 +1,27 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UserService } from './user.service';
+import * as chai from 'chai';
+import chaiHttp = require('chai-http');
+import { REDIS_PROVIDER } from '../database/database.constant';
+chai.use(chaiHttp);
 
 describe('UserService', () => {
   let service: UserService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UserService],
+      providers: [
+        UserService,
+        {
+          provide: 'USER_REPOSITORY',
+          useValue: {},
+        },
+
+        {
+          provide: REDIS_PROVIDER,
+          useValue: {},
+        },
+      ],
     }).compile();
 
     service = module.get<UserService>(UserService);
